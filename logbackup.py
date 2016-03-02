e0adf059d834051719a9507e94c799cd4e93e914
herocjx <herocjx@163.com>
2016-03-02 22:52:06 +0800

    pycharm

--- logbackup.py
+++ logbackup.py
@@ -1,18 +1,25 @@
 #!/usr/bin/python
 
-import os
+import os, shutil
 
-listdir = os.listdir('.')
-for x in listdir:
-    if os.path.isdir(x):
-    	print x
+applogs = ['/app/yxpt1.0/msgsave/main/logs/',
+	'/app/yxpt1.0/synco2o/main/logs/']
+
+def movefile(s,d):
+    filename = os.listdir(s)
+    for f in filename:
+        shutil.move(s + f, d)
+
+def tarfiel(s,f):
+    os.chdir(s)
+    shutil.make_archive(f, 'gztar', f)
+    os.rmdir(f)
 
 
-os.chdir('/var/log')
+if __name__ == '__main__':
+    import datetime
+    yesterday = datetime.datetime.today()
+    for x in applogs:
+        movefile(x, '/logsbackup/msgsave/' + yest)
+        tarfiel('/logsbackup/msgsave/', yest)
 
-listsdir = os.listdir('.')
-
-for y in listsdir:
-    print y
-
-print "all"
